a225dafd7093643fdd72f6a9aa1b20482b796329
tlamen <bernardolobo3@gmail.com>
2025-01-20 20:45:49 -0300

    qt gui test

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module IP_sim
 go 1.23.3
 
 require (
+	github.com/therecipe/qt v0.0.0-20200904063919-c0c124a5770d
+	gonum.org/v1/plot v0.15.0
+)
+
+require (
 	git.sr.ht/~sbinet/gg v0.6.0 // indirect
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/campoy/embedmd v1.0.0 // indirect
@@ -10,9 +15,8 @@ require (
 	github.com/go-latex/latex v0.0.0-20240709081214-31cef3c7570e // indirect
 	github.com/go-pdf/fpdf v0.9.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20190411002643-bd77b112433e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/image v0.21.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
-	gonum.org/v1/gonum v0.15.1 // indirect
-	gonum.org/v1/plot v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,38 +1,69 @@
 package main
 
 import (
+	"IP_sim/common/communication"
 	"fmt"
 	"os"
-	"IP_sim/common/communication"
 	"sync"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Uso: go run main.go [client|server]")
-        return
-    }
-
-    var wg sync.WaitGroup
-    wg.Add(1)
-
-    switch os.Args[1] {
-    case "client":
-	var message string
-	// Se comando no terminal tiver uma mensagem
-	if len(os.Args) > 2 {
-		message = os.Args[2]
-	} else { // Se a mensagem não for passada no comando
-		fmt.Println("Digite a mensagem para o servidor:")
-		fmt.Scanln(&message)
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: go run main.go [client|server]")
+		return
 	}
-        go communication.StartClient(&wg, message)
-    case "server":
-        go communication.StartServer(&wg)
-    default:
-        fmt.Println("Argumento inválido. Use 'client' ou 'server'.")
-        wg.Done()
-    }
 
-    wg.Wait() // Aguarda todas as goroutines finalizarem
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+	window := widgets.NewQMainWindow(nil, 0)
+
+	switch os.Args[1] {
+	case "client":
+
+		window.SetWindowTitle("Cliente")
+
+		var message string
+		// Se comando no terminal tiver uma mensagem
+		if len(os.Args) > 2 {
+			message = os.Args[2]
+		}
+		label := widgets.NewQLabel(nil, 0)
+		label.SetText(fmt.Sprintf("Digite a mensagem para o servidor:"))
+
+		// input := widgets.NewQLineEdit(message)
+		// input.SetPlaceholderText("Mensagem")
+
+		window.SetCentralWidget(label)
+
+		// Show the window
+		window.Resize(core.NewQSize2(400, 300))
+		window.Show()
+
+		// Start the application
+		app.Exec()
+		go communication.StartClient(&wg, message)
+	case "server":
+		window.SetWindowTitle("Servidor")
+
+		label := widgets.NewQLabel(nil, 0)
+		window.SetCentralWidget(label)
+
+		// Show the window
+		window.Resize(core.NewQSize2(400, 300))
+		window.Show()
+
+		// Start the application
+		app.Exec()
+
+		go communication.StartServer(&wg)
+	default:
+		fmt.Println("Argumento inválido. Use 'client' ou 'server'.")
+		wg.Done()
+	}
+
 }
